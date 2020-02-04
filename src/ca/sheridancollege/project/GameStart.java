41daade84942bcdc870b7458966d979a5dfc780b
mohd009 <abdulnmo@sheridancollege.ca>
2020-02-04 01:01:10 -0500

    New class to start game

---
+++ src/ca/sheridancollege/project/GameStart.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author User
+ */
+public class GameStart extends Game {
+
+    public GameStart(String game){
+        super(game);
+    }
+    /**
+     * this method asks for how many users will play
+     * the game minimum at-least two and max is 4
+     */
+    public void initializePlayers(){
+        
+    }
+    
+    @Override
+    public void declareWinner(){
+        
+    }
+    @Override
+    public void play(){
+        
+    }
+    
+    
+}

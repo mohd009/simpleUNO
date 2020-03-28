cfda47241a792ffec090a2d78c6e0ca70bc10dd7
mohd009 <abdulnmo@sheridancollege.ca>
2020-03-28 04:26:55 -0400

    testing file

---
+++ src/GameCreationtoPlay/GameBegin.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameCreationtoPlay;
+
+import PlayersAndCards.GroupOfCards;
+import java.util.ArrayList;
+import PlayersAndCards.Player;
+
+/**
+ *
+ * @author User
+ */
+public class GameBegin {
+    public static void main(String[] args) {
+     //   CreateGame players = new CreateGame();
+      //   players.play();
+         GroupOfCards c = new GroupOfCards(10);
+      
+         
+         c.assignCards();
+
+ GroupOfCards cd = new GroupOfCards(10);
+      
+       
+         cd.assignCards();         
+         
+         
+         
+         
+         
+        
+        
+        
+    }
+    
+}

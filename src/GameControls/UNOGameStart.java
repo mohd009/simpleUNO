a8fe344fb8de541b411e7e9ff49e7e6109170bf0
mohd009 <abdulnmo@sheridancollege.ca>
2020-03-28 04:27:28 -0400

    run the game logic class

---
+++ src/GameControls/UNOGameStart.java
@@ -0,0 +1,26 @@
+package GameControls;
+
+import PlayersAndCards.Game;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ * Shows view of game and start the game engine
+ */
+
+public class UNOGameStart {
+
+    public UNOGameStart(String game){
+        
+    }
+    
+    
+    
+    
+}
